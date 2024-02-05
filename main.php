3da414d3fce28a0371ffa708bc8e1c316f67b2ca
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2024-02-05 14:13:59 +0200

    nuorodų sistema žodžiais

--- main.php
+++ main.php
@@ -1,3 +1,30 @@
 <?php
 
 	echo 'pradedam realizuoti serverio (back-end) dalį';
+	/*
+	nuorodų sistema patikrink ar vartotojas atsiuntė naują nuorodą
+	
+		jei atsiuntė naują nuorodą tai nuorodu sistema išsaugok ją
+		
+	nuorodų sistema patikrink ar vartotojas atsiuntė pakoreguota esamą nuorodą ir jos duomenis
+	
+		jei atsiuntė pakoreguota esamą nuorodą ir jos duomenis, nuorodu sistema  išsaugok naujus nuorodos duomenis
+		
+	nuorodų sistema patikrink ar vartotojas nurodė pašalinti nuorodą
+	
+		jei nurodė tai nuorodų sistema pašalink nuorodą
+		
+	nuorodų sistema patikrink ar vartotojas uždavė paieškos kriterijus
+	
+		jei uždavė tai 
+		
+			nuorodų sistema pasiimk paieškos kriterijus
+		
+			nuorodu sistema atrink nuorodas pagal paieškos kriterijus
+		
+		jei neuždavė
+
+			nuorodų sistema pasiimk pradinį nuorodų sarašą
+	*/	
+	
+		
